fc3204ecc842877b66e9ca1f291a92ad92bb9061
Z3NJIN <marlondvg@gmail.com>
2018-03-30 11:45:25 -0500

    Add files via upload

---
+++ src/app/book.model.ts
@@ -0,0 +1,9 @@
+//Exportamos esta clase
+export class Book{
+    constructor(
+        public id: number,
+        public title: string,
+        public author: string,
+        public url?: string
+    ){}
+}
